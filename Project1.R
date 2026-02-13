43665bfe0eb304c2b6ac43bb7633a9b3c6a09f57
taylorsenters <taylorsenters@gmail.com>
2026-02-12 21:58:58 -0500

    updated to complete assignment

--- Project1.R
+++ Project1.R
@@ -3,7 +3,7 @@
 # important -- this makes sure our runs are consistent and reproducible
 set.seed(0)
 
-file <- "TCGA_breast_cancer_LumA_vs_Basal_PAM50.tsv"
+file <- "TCGA_breast_cancer_ERpositive_vs_ERnegative_PAM50.tsv"
 first10 <- c('NAT1','BIRC5','BAG1','BCL2','BLVRA','CCNB1','CCNE1','CDC6','CDC20','CDH3')
 nfold=5
   
@@ -15,48 +15,64 @@ names(data) <- header
 
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t", what = character())
 
-LumA <- data[data$gene_id %in% first10,header2=='Luminal A']
-Basal <- data[data$gene_id %in% first10,header2=='Basal-like']
+Positive <- data[data$gene_id %in% first10,header2=='Positive']
+Negative <- data[data$gene_id %in% first10,header2=='Negative']
 
-
-# define function cross_valid so we can rerun the cross validataion with various parameters
-#cross_validation <- function (nfold, alg="centroid") {
 
   # split each cancer type samples into nfold groups
-  LumA_groups <- split(sample(colnames(LumA)), 1+(seq_along(colnames(LumA)) %% nfold))
-  Basal_groups <- split(sample(colnames(Basal)), 1+(seq_along(colnames(Basal)) %% nfold))
+  Positive_groups <- split(sample(colnames(Positive)), 1+(seq_along(colnames(Positive)) %% nfold))
+  Negative_groups <- split(sample(colnames(Negative)), 1+(seq_along(colnames(Negative)) %% nfold))
   
-  result <- array()
+  centroid_result <- array()
+  glm_result <- array()
  
   # iterate from 1 to nfold groups -- to choose test group
   for (test_group in 1:nfold) {
    
     # return all samples in the chosen test group
-    testLumA <- LumA[,colnames(LumA) %in% unlist(LumA_groups[test_group])]
-    testBasal <- Basal[,colnames(Basal) %in% unlist(Basal_groups[test_group])]
-   
+    testPositive <- Positive[,colnames(Positive) %in% unlist(Positive_groups[test_group])]
+    testNegative <- Negative[,colnames(Negative) %in% unlist(Negative_groups[test_group])]
+    
     # return all samples *not* in the chosen test group 
-    trainingLumA <- LumA[,!(colnames(LumA) %in% unlist(LumA_groups[test_group]))]
-    trainingBasal <- Basal[,!(colnames(Basal) %in% unlist(Basal_groups[test_group]))]
-   
+    trainingPositive <- Positive[,!(colnames(Positive) %in% unlist(Positive_groups[test_group]))]
+    trainingNegative <- Negative[,!(colnames(Negative) %in% unlist(Negative_groups[test_group]))]
+
     # compute centroid for each cancer type -- mean for each gene based on all samples
     # note -- rows are gene
-    centroidLumA <- rowMeans(trainingLumA)
-    centroidBasal <- rowMeans(trainingBasal)
-   
+    centroidPositive <- rowMeans(trainingPositive)
+    centroidNegative <- rowMeans(trainingNegative)
+
+    #kNNC
     # For each sample in the test set decide whether it will be classified
-    # distance from centroid Lum A: sum(abs(x-centroidLumA))
-    # distance from centroid Basal: sum(abs(x-centroidBasal))
-    # distance is a sum of distances over all genes 
+    # distance from centroid Positive: sum(abs(x-centroidPositive))
+    # distance from centroid Negative: sum(abs(x-centroidNegative))
+    # distance is a sum of distances over all genes
     # misclassification if when the distance is greater from centroid associated with known result
-    misclassifiedLumA <- sum(sapply(testLumA, function(x) { sum(abs(x-centroidLumA))>sum(abs(x-centroidBasal)) }))
-    misclassifiedBasal <- sum(sapply(testBasal, function(x) { sum(abs(x-centroidLumA))<sum(abs(x-centroidBasal)) }))
+    centroid_misclassifiedPositive <- sum(sapply(testPositive, function(x) { sum(abs(x-centroidPositive))>sum(abs(x-centroidNegative)) }))
+    centroid_misclassifiedNegative <- sum(sapply(testNegative, function(x) { sum(abs(x-centroidPositive))<sum(abs(x-centroidNegative)) }))
+
+    # GLM
+    # prepare/transform training data
+    glm_training_data <- rbind(cbind(data.frame(t(trainingPositive)), cancer=0), cbind(data.frame(t(trainingNegative)), cancer=1))
+    # transform test data
+    glm_test_data_positive <- data.frame(t(testPositive))
+    glm_test_data_negative <- data.frame(t(testNegative))
+    # fit model
+    glm_model <- glm(cancer~., data=glm_training_data, family=binomial)
+    # make predictions
+    glm_p_positive <- predict(glm_model, newdata=glm_test_data_positive, type="response")
+    glm_p_negative <- predict(glm_model, newdata=glm_test_data_negative, type="response")
+    # calculate misclassified
+    glm_misclassifiedPositive <- sum(ifelse(glm_p_positive<0.5,0,1))
+    glm_misclassifiedNegative <- sum(ifelse(glm_p_negative>=0.5,0,1))
     
-    result[test_group] <- (misclassifiedLumA+misclassifiedBasal)/(ncol(testLumA)+ncol(testBasal))
+    centroid_result[test_group] <- (centroid_misclassifiedPositive+centroid_misclassifiedNegative)/(ncol(testPositive)+ncol(testNegative))
+    glm_result[test_group] <- (glm_misclassifiedPositive+glm_misclassifiedNegative)/(ncol(testPositive)+ncol(testNegative))
  }
  
- c(mean(result), sd(result))
-#}
+ c(mean(centroid_result), sd(centroid_result))
+ c(mean(glm_result), sd(glm_result))
+
 
 #x<-data.frame(three=cross_validation(3), five=cross_validation(5), ten=cross_validation(10))
 #rownames(x) <- c('mean','sd')
--- Project1fs.R
+++ Project1fs.R
@@ -1,10 +1,11 @@
+library(glmnet)
+
 # important -- this makes sure our runs are consistent and reproducible
 set.seed(0)
 
 file <- "TCGA_breast_cancer_ERstatus_allGenes.txt"
 nfold <- 5
-sd_threashold <- 4
-top_num <- 10
+sd_threashold <- 1
   
 header <- scan(file, nlines = 1, sep="\t", what = character())
 data <- read.table(file, skip = 2, header = FALSE, sep = "\t", quote = "", check.names=FALSE)
@@ -21,13 +22,13 @@ data_sd<-sapply(seq(nrow(data)), function(x) { as.numeric(sd(data[x,-1])) })
 # add gene names to the sd list
 data_sd_names<-cbind(data.frame(data_sd),data[,1])
 
-# create an "include" list of all those genes where sd > threashold
+# create an "include" list of all those genes where sd > threshold
 include_list <- data_sd_names[data_sd_names[,1]>sd_threashold,2]
 
 Positive <- data[data$gene_id %in% include_list,header2=='Positive']
 Negative <- data[data$gene_id %in% include_list,header2=='Negative']
 
-# define function cross_valid so we can rerun the cross validataion with various parameters
+# define function cross_valid so we can rerun the cross validation with various parameters
 cross_validation <- function (nfold, alg="centroid") {
 
   Positive_groups <- split(sample(colnames(Positive)), 1+(seq_along(colnames(Positive)) %% nfold))
@@ -37,41 +38,107 @@ cross_validation <- function (nfold, alg="centroid") {
   
   for (test_group in 1:nfold) {
     
-    testA <- Positive[,colnames(Positive) %in% unlist(Positive_groups[test_group])]
-    testB <- Negative[,colnames(Negative) %in% unlist(Negative_groups[test_group])]
+    testPos <- Positive[,colnames(Positive) %in% unlist(Positive_groups[test_group])]
+    testNeg <- Negative[,colnames(Negative) %in% unlist(Negative_groups[test_group])]
     
-    trainingA <- Positive[,!(colnames(Positive) %in% unlist(Positive_groups[test_group]))]
-    trainingB <- Negative[,!(colnames(Negative) %in% unlist(Negative_groups[test_group]))]
+    trainingPos <- Positive[,!(colnames(Positive) %in% unlist(Positive_groups[test_group]))]
+    trainingNeg <- Negative[,!(colnames(Negative) %in% unlist(Negative_groups[test_group]))]
     
     # Feature selection -- 
     
     # compute t-statistic for each row
-    training_t_stat<-data.frame(sapply(seq(nrow(trainingA)), function(x) { abs(as.numeric(t.test(trainingA[x,], trainingB[x,])$statistic)) }))
+    training_t_stat<-data.frame(sapply(seq(nrow(trainingPos)), function(x) { abs(as.numeric(t.test(trainingPos[x,], trainingNeg[x,])$statistic)) }))
     
     # add gene id column
-    training_t_stat_geneid<-cbind(training_t_stat,rownames(trainingA))
+    training_t_stat_geneid<-cbind(training_t_stat,rownames(trainingPos))
     colnames(training_t_stat_geneid) <- c('t','id')
     
     # pick top 50 based on t-statistic
     selected_genes <- head(training_t_stat_geneid[order(-training_t_stat_geneid$t),],n=top_num)[,2]
     
     # narrow down the list of genes based on t-statistic
-    testA <- testA[rownames(testA) %in% selected_genes,]
-    testB <- testB[rownames(testB) %in% selected_genes,]
-    trainingA <- trainingA[rownames(trainingA) %in% selected_genes,]
-    trainingB <- trainingB[rownames(trainingB) %in% selected_genes,]
+    testPos <- testPos[rownames(testPos) %in% selected_genes,]
+    testNeg <- testNeg[rownames(testNeg) %in% selected_genes,]
+    trainingPos <- trainingPos[rownames(trainingPos) %in% selected_genes,]
+    trainingNeg <- trainingNeg[rownames(trainingNeg) %in% selected_genes,]
     
-    centroidA <- rowMeans(trainingA)
-    centroidB <- rowMeans(trainingB)
+    if (alg == "centroid") {
+      centroidPos <- rowMeans(trainingPos)
+      centroidNeg <- rowMeans(trainingNeg)
+      
+      misclassifiedPos <- sum(sapply(testPos, function(x) { sqrt(sum((x-centroidPos)^2))-sqrt(sum((x-centroidNeg)^2))>0 }))
+      misclassifiedNeg <- sum(sapply(testNeg, function(x) { sqrt(sum((x-centroidPos)^2))-sqrt(sum((x-centroidNeg)^2))<0 }))
+    }
     
-    misclassifiedA <- sum(sapply(testA, function(x) { sqrt(sum((x-centroidA)^2))-sqrt(sum((x-centroidB)^2))>0 }))
-    misclassifiedB <- sum(sapply(testB, function(x) { sqrt(sum((x-centroidA)^2))-sqrt(sum((x-centroidB)^2))<0 }))
+    if (alg == "GLM") {
+      # format training data
+      training_data <- rbind(cbind(data.frame(t(trainingPos)), cancer=0), cbind(data.frame(t(trainingNeg)), cancer=1))
+      
+      # format test data
+      test_data_pos <- data.frame(t(testPos))
+      test_data_neg <- data.frame(t(testNeg))
+      
+      # fit model
+      model <- glm(cancer~., data=training_data, family=binomial)
+      
+      # make predictions
+      p_positive <- predict(model, newdata=test_data_pos, type="response")
+      p_negative <- predict(model, newdata=test_data_neg, type="response")
+      
+      # calculate misclassified
+      misclassifiedPos <- sum(ifelse(p_positive < 0.5, 0, 1))
+      misclassifiedNeg <- sum(ifelse(p_negative >= 0.5, 0, 1))
+    }
     
-    result[test_group] <- (misclassifiedA+misclassifiedB)/(ncol(testA)+ncol(testB))
+    if (alg == "Lasso") {
+      # format training data 
+      # glmnet requires a matrix 'x' (genes) and a vector 'y' (cancer labels)
+      x_train <- rbind(t(trainingPos), t(trainingNeg))
+      y_train <- c(rep(0, ncol(trainingPos)), rep(1, ncol(trainingNeg)))
+      
+      # format test data (must be converted to matrix for glmnet)
+      test_data_pos <- as.matrix(t(testPos))
+      test_data_neg <- as.matrix(t(testNeg))
+      
+      # fit model
+      # cv.glmnet automatically solves the convergence/error issues
+      # alpha=1 means Lasso
+      model <- cv.glmnet(as.matrix(x_train), y_train, family="binomial", alpha=1)
+      
+      # make predictions
+      # use s="lambda.min" to use the best penalty found
+      p_positive <- predict(model, newx=test_data_pos, s="lambda.min", type="response")
+      p_negative <- predict(model, newx=test_data_neg, s="lambda.min", type="response")
+      
+      # calculate misclassified
+      misclassifiedPos <- sum(ifelse(p_positive < 0.5, 0, 1))
+      misclassifiedNeg <- sum(ifelse(p_negative >= 0.5, 0, 1))
+    }
+    
+    result[test_group] <- (misclassifiedPos+misclassifiedNeg)/(ncol(testPos)+ncol(testNeg))
   }
  
   paste0(round(mean(result),4)," sd=(",round(sd(result),4),")")
 }
 
-kable(cross_validation(5))
+# Update top_num to 50
+top_num <- 50
+results_50_centroid <- cross_validation(5, alg="centroid")
+results_50_glm <- cross_validation(5, alg="GLM")
+results_50_lasso <- cross_validation(5, alg="Lasso")
+
+# Update top_num to 100
+top_num <- 100
+results_100_centroid <- cross_validation(5, alg="centroid")
+results_100_glm <- cross_validation(5, alg="GLM")
+results_100_lasso <- cross_validation(5, alg="Lasso")
+
+# Create a final comparison table
+fs_summary <- data.frame(
+  "Genes" = c("Top 50", "Top 50", "Top 50", "Top 100", "Top 100", "Top 100"),
+  "Alg"   = c("Centroid", "GLM", "Lasso", "Centroid", "GLM", "Lasso"),
+  "Result" = c(results_50_centroid, results_50_glm, results_50_lasso, results_100_centroid, results_100_glm, results_100_lasso)
+)
+
+kable(fs_summary)
 
